347e6cbf0d8af6046b0e504b2fe2d6914a000ac5
Mukesh Maurya <mukesh.india9@gmail.com>
2024-04-07 16:32:11 +0530

    sync

---
+++ demo.js
@@ -0,0 +1,38 @@
+// let arr = [1, 0, 2, 3, 2, 3, 0, 0];
+
+// let obj = {};
+// for (let ele of arr) {
+//   if (obj[ele]) {
+//     obj[ele] += 1;
+//   } else {
+//     obj[ele] = 1;
+//   }
+// }
+// console.log(obj);
+
+// let arr = [2, 3, 2, 1, 0, 3, 0];
+
+// let outputArr = [];
+// for (let ele of arr) {
+//   if (!outputArr.includes(ele)) {
+//     outputArr.push(ele);
+//   }
+// }
+// console.log(outputArr);
+           
+// let arr = [0, 3, 2, 3, 5, 5, 9];
+// let newArr = [];
+// function findRightPosition(arr) {
+//   for (let i in arr) {
+//     console.log("i",typeof(i))
+//     console.log("arr -i",typeof arr[i])
+//     if (arr[i] == i) {
+//       newArr.push(true);
+//     } else {
+//       newArr.push(false);
+//     }
+//   }
+//   return newArr;
+// }
+
+// console.log(findRightPosition(arr));
