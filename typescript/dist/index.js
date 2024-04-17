88e6dec22a2529ab21ea70eb63867b8dffea1c13
Mukesh Maurya <mukesh.india9@gmail.com>
2024-04-17 22:41:32 +0530

    setup

---
+++ typescript/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let a = 3;
+let b = "3";
+let c = 4;
+// console.log(a/b);
+console.log(c / a);
---
+++ typescript/src/index.ts
@@ -0,0 +1,6 @@
+let a = 3;
+let b:string = "3"
+let c= 4
+
+// console.log(a/b);
+console.log(c/a)
