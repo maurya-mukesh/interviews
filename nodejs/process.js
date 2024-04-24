4f5a8c229b75cddc941119706fc049fd6312f033
Mukesh Maurya <mukesh.india9@gmail.com>
2024-04-24 23:34:49 +0530

    process module

---
+++ nodejs/process.js
@@ -0,0 +1,11 @@
+console.log(process.env.LOGNAME);
+// console.log(process);
+
+//pid
+console.log(process.pid);
+
+//cwd()
+console.log(process.cwd());
+
+//memoryUsage()
+console.log(process.memoryUsage());
